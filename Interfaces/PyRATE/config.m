25bc3ff69c842f02744029142164c8b91ff5c512
LSartore <sartore.lohan@gmail.com>
2024-03-29 13:58:12 +0100

    Updated .gitignore

--- Interfaces/PyRATE/config.m
+++
@@ -1,8 +0,0 @@
-(* ::Package:: *)
-(* ::Text:: *)
-(* This is a config file for the FeynRules - PyR@TE 3 interface. *)
-(* It contains the paths to Python's executable, PyR@TE and PyLie. *)
-
-PR$PythonPath = "";
-PR$PyratePath = "";
-PR$PyLiePath = ""
